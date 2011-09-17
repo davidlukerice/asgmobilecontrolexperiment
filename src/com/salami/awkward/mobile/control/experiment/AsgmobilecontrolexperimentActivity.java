63b4e9f7055c5e2536542c6e51abacdaf56e2276
liger13 <liger13@gatlapwin7>
2011-09-17 14:15:00 -0500

    Initial Commit

---
+++ src/com/salami/awkward/mobile/control/experiment/AsgmobilecontrolexperimentActivity.java
@@ -0,0 +1,13 @@
+package com.salami.awkward.mobile.control.experiment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AsgmobilecontrolexperimentActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
