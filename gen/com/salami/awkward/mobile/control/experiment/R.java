746998a3a70a5f64f39ef85a81b76f9273a2efc1
MSmithy42 <MSmithy42@localhost>
2011-09-17 19:53:29 -0500

    fixed typo

--- gen/com/salami/awkward/mobile/control/experiment/R.java
+++ gen/com/salami/awkward/mobile/control/experiment/R.java
@@ -23,7 +23,7 @@ public final class R {
         public static final int main=0x7f030000;
         public static final int segmentedcontrol=0x7f030001;
         public static final int tilt=0x7f030002;
-        public static final int vitualjoystick=0x7f030003;
+        public static final int virtualjoystick=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
