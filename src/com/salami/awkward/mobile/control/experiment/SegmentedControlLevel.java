b47b6425a8902bf7a297953f2ce0a5750ca324b6
hansencj06 <hansencj06@localhost>
2011-09-17 19:42:23 -0500

    added new files

---
+++ src/com/salami/awkward/mobile/control/experiment/SegmentedControlLevel.java
@@ -0,0 +1,15 @@
+package com.salami.awkward.mobile.control.experiment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SegmentedControlLevel extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.segmentedcontrol);
+	}
+
+}
