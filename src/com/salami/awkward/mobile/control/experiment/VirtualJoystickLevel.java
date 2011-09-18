79b82a473189b937fab1564a93175b3920374c91
MSmithy42 <MSmithy42@localhost>
2011-09-17 19:53:29 -0500

    fixed typo

--- src/com/salami/awkward/mobile/control/experiment/VirtualJoystickLevel.java
+++ src/com/salami/awkward/mobile/control/experiment/VirtualJoystickLevel.java
@@ -9,7 +9,7 @@ public class VirtualJoystickLevel extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.vitualjoystick);
+		setContentView(R.layout.virtualjoystick);
 	}
 
 }
