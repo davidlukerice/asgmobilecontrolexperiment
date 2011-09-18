2a5981a488074b2e4f1e2a9e78847845afdafc12
drice <drice@localhost>
2011-09-17 20:33:25 -0500

    Added andEngine sources

--- src/com/salami/awkward/mobile/control/experiment/TiltLevel.java
+++ src/com/salami/awkward/mobile/control/experiment/TiltLevel.java
@@ -2,6 +2,7 @@ package com.salami.awkward.mobile.control.experiment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
 
 public class TiltLevel extends Activity{
 
