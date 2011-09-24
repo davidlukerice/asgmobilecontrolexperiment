43ac8958abcb3b9f6194244c9b49ed7afa05a1a8
tim <tim@localhost>
2011-09-23 21:41:50 -0500

    A bunch of different stuff: -Made some inconsequential changes to Hero.java -SegmentedControlLevel uses Hero.java -Added new layer of abstraction with ControlScheme stuff since it seems like we'd need it eventually anyway and it was the cleanest way I could think of to fix the "velocity only gets reset when you actually touch or push on the screen" problem -Fixed the "velocity only gets reset when you actually touch or push on the screen" problem

---
+++ src/com/salami/awkward/mobile/control/experiment/IControlScheme.java
@@ -0,0 +1,8 @@
+package com.salami.awkward.mobile.control.experiment;
+
+import org.anddev.andengine.engine.handler.IUpdateHandler;
+
+public interface IControlScheme extends IUpdateHandler{
+
+	
+}
