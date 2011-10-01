f382dae7678a13fc3431bfbd533b106ec70a6eb4
tim <tim@localhost>
2011-10-01 08:57:10 -0500

    Changed control scheme interface so it'll be easier to use in a single activity for different control schemes when we make one.

--- src/com/salami/awkward/mobile/control/experiment/IControlScheme.java
+++ src/com/salami/awkward/mobile/control/experiment/IControlScheme.java
@@ -1,8 +1,14 @@
 package com.salami.awkward.mobile.control.experiment;
 
 import org.anddev.andengine.engine.handler.IUpdateHandler;
+import org.anddev.andengine.entity.scene.Scene;
 
 public interface IControlScheme extends IUpdateHandler{
-
 	
+	/** Register listeners for control events such as tilt, touch, etc.
+	 * This needs to be in the subclass since each one will listen for
+	 * different events
+	 * @param mScene
+	 */
+	public void registerListeners(Scene mScene);
 }
